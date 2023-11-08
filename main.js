7f865a875a1ac5b1767ab552f295f2d00ff458fc
Guilherme234521 <112871023+Guilherme234521@users.noreply.github.com>
2023-11-08 20:08:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Alura MIDI</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/bateria.png">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="sytlesheet" href="https://cdn1.gnarususercontent.com.br/1/787848/8b1f66dc-900b-4f87-8ac0-2d1255ae1071.png">
+
+</head>
+
+<body>
+
+    <h1>Projeto pacheco</h1>
+
+    <section class="teclado">
+        <button class="tecla tecla_1">
+            <span class="tecla_title">1</span>
+        </button>
+
+        <button class="tecla tecla_2">
+            <span class="tecla_title">2</span>
+        </button>
+
+        <button class="tecla tecla_3">
+            <span class="tecla_title">3</span>
+        </button>
+
+
+        <button class="tecla tecla_4">
+            <span class="tecla_title">4</span>
+        </button>
+
+        <button class="tecla tecla_5">
+            <span class="tecla_title">5</span>
+        </button>
+
+        <button class="tecla tecla_6">
+            <span class="tecla_title">6</span>
+        </button>
+
+
+        <button class="tecla tecla_7">
+            <span class="tecla_title">7</span>
+        </button>
+
+        <button class="tecla tecla_8">
+            <span class="tecla_title">8</span>
+        </button>
+
+        <button class="tecla tecla_9">
+            <span class="tecla_title">9</span>
+        </button>
+    </section>
+
+    <audio src="sounds/key1.wav" id="som_tecla_1"></audio>
+    <audio src="sounds/key2.wav" id="som_tecla_2"></audio>
+    <audio src="sounds/key3.wav" id="som_tecla_3"></audio>
+    <audio src="sounds/key4.wav" id="som_tecla_4"></audio>
+    <audio src="sounds/key5.wav" id="som_tecla_5"></audio>
+    <audio src="sounds/key6.wav" id="som_tecla_6"></audio>
+    <audio src="sounds/key7.wav" id="som_tecla_7"></audio>
+    <audio src="sounds/key8.wav" id="som_tecla_8"></audio>
+    <audio src="sounds/key9.wav" id="som_tecla_9"></audio>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
